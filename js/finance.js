b417ee18f62082620e9eaa8babea33d7f648d7d6
openvault <157576678+mazharmecci@users.noreply.github.com>
2025-12-31 14:54:35 +0530

    Update finance.js

--- js/finance.js
+++ js/finance.js
@@ -267,10 +267,10 @@ function renderFinanceList() {
           <div class="fee-table-wrapper">
             <table class="fee-table">
               <colgroup>
+                <col style="width:25%">
+                <col style="width:18%">
+                <col style="width:24%">
                 <col style="width:30%">
-                <col style="width:20%">
-                <col style="width:25%">
-                <col style="width:25%">
               </colgroup>
               <thead>
                 <tr>
